e4cb7ea6a7f666d48f75815060540fbb31947866
tjsoliveira <thiagocorey@gmail.com>
2017-10-27 11:17:24 -0300

    Try Django 1.10 - 27 of 40 - Django Forms

--- shortener/views.py
+++ shortener/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 
+from .forms import SubmitUrlForm
 from .models import KirrUrl
 
 class HomeView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'shortener/home.html', {})
+        form = SubmitUrlForm()
+        context = {
+            'title': 'Submit URL',
+            'form': form
+        }
+        return render(request, 'shortener/home.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = SubmitUrlForm()
+        if form.is_valid():
+            print(form.cleaned_data)
+
+        context = {
+            'title': 'Submit URL',
+            'form': form
+        }
+        return render(request, 'shortener/home.html', context)
 
 class KirrCBView(View):
 
